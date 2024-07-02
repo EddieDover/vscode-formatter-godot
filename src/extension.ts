d65a2bd34b94e6b173dfefe2faa9f8b7094cad11
Eddie Dover <ed@eddiedover.dev>
2024-07-01 20:59:18 -0700

    chore: restructured extension to increase both functionality and general speed.

--- src/extension.ts
+++ src/extension.ts
@@ -1,93 +1,170 @@
-import * as cp from 'child_process';
-import * as os from 'os';
-import * as vscode from 'vscode';
+import * as cp from "child_process";
+import * as os from "os";
+import * as vscode from "vscode";
 
-var ochan: vscode.OutputChannel;
-const matchregex: RegExp = /(\w+\.gd):(\d+):\s?Error\:\s?(.+)?/g;
+export const matchRegexError: RegExp = /(\w+\.gd):(\d+):\s?Error:\s?(.+)?/g;
+export const matchRegexToken: RegExp = /(.+) at line (\d+), column (\d+)\./gm;
+export const matchRegexTokenFile: RegExp = /['|"]?(.+\.gd):['|"]?/g;
+
+const severityLevel = vscode.workspace
+  .getConfiguration("godotFormatterAndLinter")
+  .get("lintSeverityLevel", "Error");
+
+let timeout: NodeJS.Timeout | undefined = undefined;
+
+export const scanLineForGeneralError = (
+  line: string,
+  diagArr: any[],
+  code: string,
+  ochan: vscode.OutputChannel
+) => {
+  let match = matchRegexError.exec(line);
+  if (match) {
+    let filename = match[1];
+    let lineno = parseInt(match[2]) - 1;
+    let message = match[3];
+    ochan.append("Error: " + filename + ":" + lineno + ": " + message + "\n");
+    const va = new vscode.Diagnostic(
+      new vscode.Range(lineno, 0, lineno, line.length - 1),
+      message,
+      vscode.DiagnosticSeverity[severityLevel]
+    );
+    va.code = code;
+    diagArr.push(va);
+  }
+};
+
+export const scanLineForTokenError = (
+  line: string,
+  diagArr: any[],
+  code: string,
+  ochan: vscode.OutputChannel
+) => {
+  let tokenFile = "";
+  let match = matchRegexTokenFile.exec(line);
+  if (match) {
+    tokenFile = match[1];
+  }
+
+  match = matchRegexToken.exec(line);
+  if (match) {
+    let message = match[1];
+    let lineno = parseInt(match[2]);
+    let colno = parseInt(match[3]);
+    ochan.append(
+      "Token: " + tokenFile + ":" + lineno + ":" + colno + ": " + message + "\n"
+    );
+    const va = new vscode.Diagnostic(
+      new vscode.Range(lineno, 0, lineno, line.length - 1),
+      message,
+      vscode.DiagnosticSeverity[severityLevel]
+    );
+    va.code = code;
+    diagArr.push(va);
+  }
+};
+
+export async function formatDocument(
+  document: vscode.TextDocument
+): Promise<vscode.TextEdit[]> {
+  let content = document.getText();
+  const indentType = vscode.workspace
+    .getConfiguration("godotFormatterAndLinter")
+    .get("indentType");
+  const indentSpacesSize = vscode.workspace
+    .getConfiguration("godotFormatterAndLinter")
+    .get("indentSpacesSize");
+  const indentParam =
+    indentType === "Tabs" ? "" : `--use-spaces=${indentSpacesSize}`;
+  const lineLength = vscode.workspace
+    .getConfiguration("godotFormatterAndLinter")
+    .get("lineLength");
+  return new Promise((res, rej) => {
+    let cmd = `gdformat --line-length=${lineLength} ${indentParam} -`;
+
+    const cpo = cp.exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        rej(err);
+      }
+      res([
+        vscode.TextEdit.replace(
+          new vscode.Range(
+            new vscode.Position(0, 0),
+            new vscode.Position(document.lineCount, 9999999)
+          ),
+          stdout
+        ),
+      ]);
+    });
+    cpo.stdin?.write(content);
+    cpo.stdin?.end(os.EOL);
+  });
+}
+
+export const lintDocument = (
+  doc: vscode.TextDocument,
+  diag: vscode.DiagnosticCollection,
+  ochan: vscode.OutputChannel
+) => {
+  let content = doc.fileName;
+  let uri = doc.uri;
+  let diagArr: vscode.Diagnostic[] = [];
+
+  if (uri.scheme === "file" && doc.languageId === "gdscript") {
+    let cmd = `gdlint "${content}" 2>&1`;
+
+    const result = cp.spawnSync(cmd, { shell: true, stdio: "pipe" });
+
+    const { stdout } = result;
+    let lines: string[] = stdout?.toString().split("\n") ?? [];
+    lines = lines.map((line) => line.trim()).filter((line) => line.length > 0);
+    lines.map((line: string) => {
+      matchRegexTokenFile.lastIndex = 0;
+      matchRegexError.lastIndex = 0;
+
+      scanLineForGeneralError(line, diagArr, "gdlint", ochan);
+      scanLineForTokenError(line, diagArr, "gdlint", ochan);
+    });
+
+    diag.clear();
+    diag.set(uri, [...diagArr]);
+  }
+  return [...diagArr];
+};
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('\'GDScript Formatter & Linter\' is now active!');
+  console.log("'GDScript Formatter & Linter' is now active!");
 
-    ochan = vscode.window.createOutputChannel("Godot Formatter");
+  const ochan = vscode.window.createOutputChannel("Godot Formatter");
+  const diag = vscode.languages.createDiagnosticCollection("gdlint");
 
-    var diag = vscode.languages.createDiagnosticCollection("gdlint");
+  context.subscriptions.push(
+    vscode.window.onDidChangeActiveTextEditor((editor) => {
+      if (editor) {
+        lintDocument(editor.document, diag, ochan);
+      }
+    })
+  );
 
-    var doLint = (doc: vscode.TextDocument) => {
-        let content = doc.fileName;
-        let uri = doc.uri;
-        let dir = vscode.workspace.getWorkspaceFolder(uri)?.uri.fsPath || "";
-        const severityLevel = vscode.workspace.getConfiguration("godotFormatterAndLinter").get("lintSeverityLevel", "Error");
-
-        if (uri.scheme === "file" && doc.languageId === 'gdscript') {
-            let cmd = `gdlint "` + content + `" 2>&1`;
-
-            var cpo = cp.exec(cmd, { cwd: dir }, (err, stdout, stderr) => {
-
-                let diagArr: vscode.Diagnostic[] = [];
-                const lines = stdout.split('\n').map((line) => line.trim()).filter((line) => line.length > 0);
-                lines.map((line: string) => {
-                    matchregex.lastIndex = 0;
-                    const match = matchregex.exec(line);
-                    if (match) {
-                        let filename = match[1];
-                        let lineno = parseInt(match[2]) - 1;
-                        let message = match[3];
-                        ochan.append("Error: " + filename + ":" + lineno + ": " + message + "\n");
-                        const va = new vscode.Diagnostic(new vscode.Range(lineno, 0, lineno, line.length - 1), message, vscode.DiagnosticSeverity[severityLevel]);
-                        va.code = "gdlint";
-                        diagArr.push(va);
-                    }
-
-                });
-                ochan.append("setting an array with " + diagArr.length + " elements as diag for URI: " + uri + '\n');
-                diag.set(uri, []);
-                diag.set(uri, diagArr);
-
-            });
-            cpo.stdin?.write(content);
-            cpo.stdin?.end(os.EOL);
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeTextDocument((e) => {
+      if (!e?.document?.isDirty) {
+        if (timeout) {
+          clearTimeout(timeout);
         }
-    };
-    //setInterval(doLint, 5000);
+        timeout = setTimeout(() => {
+          lintDocument(e.document, diag, ochan);
+        }, 300); // 300ms debounce
+      }
+    })
+  );
 
-    context.subscriptions.push(
-        vscode.window.onDidChangeActiveTextEditor(editor => {
-            if (editor) {
-                doLint(editor.document);
-            }
-        })
-    );
-
-    context.subscriptions.push(
-        vscode.workspace.onDidChangeTextDocument(e => doLint(e.document))
-    );
-
-    context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('gdscript', {
-        async provideDocumentFormattingEdits(document: vscode.TextDocument): Promise<vscode.TextEdit[]> {
-            let content = document.getText();
-            const indentType = vscode.workspace.getConfiguration("godotFormatterAndLinter").get("indentType");
-            const indentSpacesSize = vscode.workspace.getConfiguration("godotFormatterAndLinter").get("indentSpacesSize");
-            const indentParam = (indentType === "Tabs") ? "" : `--use-spaces=${indentSpacesSize}`;
-            const lineLength = vscode.workspace.getConfiguration("godotFormatterAndLinter").get("lineLength");
-            return new Promise((res, rej) => {
-                let cmd = `gdformat --line-length=${lineLength} ${indentParam} -`;
-
-                var cpo = cp.exec(cmd, (err, stdout, stderr) => {
-                    //ochan.append("stdout: " + stdout);
-                    ochan.append("stderr: " + stderr + "\n");
-                    ochan.append("err: " + err + "\n");
-                    if (err) {
-                        rej([]);
-                    }
-                    res([vscode.TextEdit.replace(new vscode.Range(new vscode.Position(0, 0), new vscode.Position(document.lineCount, 9999999)), stdout)]);
-                });
-                cpo.stdin?.write(content);
-                cpo.stdin?.end(os.EOL);
-
-            });
-        }
-    }));
+  context.subscriptions.push(
+    vscode.languages.registerDocumentFormattingEditProvider("gdscript", {
+      provideDocumentFormattingEdits: formatDocument,
+    })
+  );
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
